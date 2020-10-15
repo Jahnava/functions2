2d1771699d95c5671c259a5d1f937657675a94ca
Jahnava <jahnavalmt@gmail.com>
2020-10-15 16:33:03 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+//find the min and max of two numbers
+
+#include <iostream>
+using namespace std;
+
+//prototypes go here
+
+//no global variables
+
+int main()
+{
+	//variables
+	int min;                        //holds the current min value found
+	int max;                       //holds the current max value found
+	int counter = 0;               //number of times looped
+	int input;                      //input value read in from the user
+
+	//begin processing
+	while (counter < 5)
+
+		//read in a value from the user
+		cout << "Enter a value: ";
+	    cin >> input
+
+}
